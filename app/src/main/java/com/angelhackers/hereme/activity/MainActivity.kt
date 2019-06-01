10c0a1a7b0c84650130d867ed71aa12a157fee19
Leesohee <shfksthgml@naver.com>
2019-06-02 06:44:37 +0900

    레이아웃 아직 덜함

--- app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
+++ app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
@@ -4,11 +4,14 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
+import android.widget.TextView
 import com.angelhackers.hereme.adapter.ProductMainPagerAdapter
 import com.angelhackers.hereme.data.get.GetTestResponse
 import com.angelhackers.hereme.network.ApplicationController
 import kotlinx.android.synthetic.main.activity_main.*
 import retrofit2.Call
+import java.text.SimpleDateFormat
+import java.util.*
 
 
 class MainActivity : AppCompatActivity() {
@@ -21,8 +24,19 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         configureMainTab()
-
+        setDate()
         // getTestResponse()
+    }
+
+    private fun setDate()
+    {
+            val now = System.currentTimeMillis()
+            val date = Date(now)
+            val sdfNow = SimpleDateFormat("MM월 dd일")
+            val formatDate = sdfNow.format(date)
+            val dateNow: TextView = findViewById(R.id.txt_toolbar_main_date)
+            dateNow.setText(formatDate)
+
     }
 
     private fun configureMainTab() {
@@ -35,8 +49,8 @@ class MainActivity : AppCompatActivity() {
             (this.getSystemService(android.content.Context.LAYOUT_INFLATER_SERVICE) as LayoutInflater)
                 .inflate(R.layout.navigation_home_main, null, false)
         tl_main_home.getTabAt(0)!!.customView = navCategoryMainLayout.findViewById(R.id.rl_nav_main_home)
-        tl_main_home.getTabAt(0)!!.customView = navCategoryMainLayout.findViewById(R.id.rl_nav_main_friend_list)
-        tl_main_home.getTabAt(0)!!.customView = navCategoryMainLayout.findViewById(R.id.rl_nav_main_setting)
+        tl_main_home.getTabAt(1)!!.customView = navCategoryMainLayout.findViewById(R.id.rl_nav_main_friend_list)
+        tl_main_home.getTabAt(2)!!.customView = navCategoryMainLayout.findViewById(R.id.rl_nav_main_setting)
     }
 //    static View v; // 프래그먼트의 뷰 인스턴스
 //    @Override
