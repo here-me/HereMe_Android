d3d358034d87b7954cfb975d1133b107974572f9
Luke Kim <tjclear214@gmail.com>
2019-06-01 23:10:23 +0900

    Maps(with Error)

--- app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
+++ app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
@@ -1,16 +1,24 @@
-package com.angelhackers.hereme.activity
+package com.angelhackers.hereme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
-import com.angelhackers.hereme.R
+import android.content.Intent
+import kotlinx.android.synthetic.main.activity_main.*
 
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_notify)
+        setContentView(R.layout.activity_main)
+
+        btn_act_main_toMap.setOnClickListener {
+            val nextIntent = Intent(this, MapsActivity::class.java)
+            startActivity(nextIntent)
+        }
 
     }
+
+
 }
