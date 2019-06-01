843d930f6a5522fd27c79c251ece8773fae21502
Luke Kim <tjclear214@gmail.com>
2019-06-02 02:20:07 +0900

    GPS

--- app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
+++ app/src/main/java/com/angelhackers/hereme/activity/MainActivity.kt
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 
 import android.content.Intent
+import android.view.View
+import android.view.ViewGroup
 import kotlinx.android.synthetic.main.activity_main.*
 
 
@@ -14,11 +16,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         btn_act_main_toMap.setOnClickListener {
-            val nextIntent = Intent(this, MapsActivity::class.java)
-            startActivity(nextIntent)
+            val intent = Intent(this, MapsActivity::class.java)
+            startActivity(intent)
+            finish()
         }
-
     }
-
-
 }
